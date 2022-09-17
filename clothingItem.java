3167d1321ce7f8ba4435301c34f1e25830c192fc
JR Garbe <jrgarbe7@gmail.com>
2022-09-17 00:03:13 -0700

    init

---
+++ clothingItem.java
@@ -0,0 +1,23 @@
+public class clothingItem {
+    private String color = null;
+    private String style = null;
+    private String category = null;
+
+    public void addClothes(String ... atrribute) {
+
+    }
+
+    public void deleteClothes() {
+
+    }
+
+    public void updateClothes(String ... attribute) {
+
+    }
+
+    // main function, helper funcs likely needed.
+    public void pickOutfit() {
+        
+    }
+}
+
